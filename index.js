72eb0a2009001a17822e3280f670a6b6c29c5102
Bruce Nishihara <bnishihara@yahoo.com>
2023-07-17 16:04:30 -0700

    Edit populateBoard() to remove extra squares

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function populateBoard(size) {
     let board = document.querySelector(".board");
+    let squares = board.querySelectorAll('div');
+    squares.forEach(div => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
