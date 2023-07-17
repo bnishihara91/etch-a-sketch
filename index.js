c7973253c49a4dae35a9ebd296df0f9a3c2c8c55
Bruce Nishihara <bnishihara@yahoo.com>
2023-07-17 16:01:56 -0700

    Insert populateBoard() into changeSize()

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ function populateBoard(size) {
 
     for (let i = 0; i < 256; i++) {
         let square = document.createElement("div");
-        square.style.backgroundColor= "blue";
+        square.style.backgroundColor = "blue";
         board.insertAdjacentElement("beforeend", square);
-
     }
 }
 
 populateBoard(16);
 
 function changeSize(input) {
-    
+    populateBoard(input);
 }
